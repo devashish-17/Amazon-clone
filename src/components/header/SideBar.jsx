e145e14818b99dda7f77641e471991c67456fc04
Devashish Revadkar <ddrevadkar@gmail.com>
2023-09-10 02:50:49 +0530

    responsive sidebar

--- src/components/header/SideBar.jsx
+++ src/components/header/SideBar.jsx
@@ -4,22 +4,22 @@ import { FaUserCircle } from "react-icons/fa";
 const SideBar = () => {
     return (
         <div>
-            <span className="bg-[#283445] text-white w-full py-3 px-10 flex items-center gap-4">
-                <FaUserCircle className="text-3xl" />
-                <p className="text-xl font-semibold"> Hello, sign in </p>
+            <span className="bg-[#283445] text-white w-full py-2 md:py-3 px-6 md:px-10 flex items-center gap-2 md:gap-4">
+                <FaUserCircle className="text-2xl md:text-3xl" />
+                <p className="md:text-xl font-semibold"> Hello, sign in </p>
             </span>
 
             <div className="">
-                <h3 className="text-2xl font-semibold py-2 px-10"> Trending </h3>
+                <h3 className="md:text-2xl font-bold md:font-semibold py-1 md:py-2 pl-6 md:px-10"> Trending </h3>
                 <h2 className="sideBarHover"> Best Sellers </h2>
                 <h2 className="sideBarHover"> New Releases </h2>
                 <h2 className="sideBarHover"> Movers and Shakers </h2>
             </div>
 
-            <hr className="border border-[#d0d4da] my-2" />
+            <hr className="border border-[#d0d4da] my-1 md:my-2" />
 
             <div className="">
-                <h3 className="text-xl font-semibold py-2 px-10"> Digital Content And Devices </h3>
+                <h3 className="md:text-2xl font-bold md:font-semibold py-1 md:py-2 pl-6 md:px-10"> Digital Content And Devices </h3>
                 <h2 className="sideBarHover flex items-center justify-between"> Echo & Alexa <MdArrowForwardIos className="text-gray-500" /> </h2>
                 <h2 className="sideBarHover flex items-center justify-between"> Fire TV <MdArrowForwardIos className="text-gray-500" /> </h2>
                 <h2 className="sideBarHover flex items-center justify-between"> Kindle E-Readers & eBooks <MdArrowForwardIos className="text-gray-500" /> </h2>
@@ -28,10 +28,10 @@ const SideBar = () => {
                 <h2 className="sideBarHover flex items-center justify-between"> Amazon Prime Music <MdArrowForwardIos className="text-gray-500" /> </h2>
             </div>
 
-            <hr className="border border-[#d0d4da] my-2" />
+            <hr className="border border-[#d0d4da] my-1 md:my-2" />
 
             <div className="">
-                <h3 className="text-xl font-semibold py-2 px-10"> Shop By Category </h3>
+                <h3 className="md:text-2xl font-bold md:font-semibold py-1 md:py-2 pl-6 md:px-10"> Shop By Category </h3>
                 <h2 className="sideBarHover flex items-center justify-between"> Mobiles, Computers <MdArrowForwardIos className="text-gray-500" /> </h2>
                 <h2 className="sideBarHover flex items-center justify-between"> TV, Appliances, Electronics <MdArrowForwardIos className="text-gray-500" /> </h2>
                 <h2 className="sideBarHover flex items-center justify-between"> Men's Fashion <MdArrowForwardIos className="text-gray-500" /> </h2>
